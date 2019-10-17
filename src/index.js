017e50290e48208f3a13a19c2bcea696e5439790
Secret <toontown@inbox.ru>
2019-10-17 17:52:17 +0300

    5. Личный проект: начинаем программировать

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./components/app/app.jsx";
+
+const init = () => {
+  ReactDOM.render(
+      <App/>,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
