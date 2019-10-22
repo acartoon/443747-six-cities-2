a9af282551e99ea42a556572c6399ff92a67a8d3
Secret <toontown@inbox.ru>
2019-10-22 17:25:40 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Main from "../main/main.jsx";
+import PropTypes from "prop-types";
 
-const App = () => {
-  return <Main/>;
+const App = (props) => {
+  const {placesList} = props;
+  return <Main
+    placesList = {placesList}/>;
+};
+
+App.propTypes = {
+  placesList: PropTypes.array.isRequired,
 };
 
 export default App;
